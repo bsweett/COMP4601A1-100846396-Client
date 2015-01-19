5aae64d5a0c91e2df1b75b7976c5358c67597289
bensweett <benjamin.sweett@gmail.com>
2015-01-19 16:02:54 -0500

    Initial Commit

---
+++ COMP4601A1-100846396-ClientTests/COMP4601A1_100846396_ClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  COMP4601A1_100846396_ClientTests.swift
+//  COMP4601A1-100846396-ClientTests
+//
+//  Created by Ben Sweett on 2015-01-19.
+//  Copyright (c) 2015 Ben Sweett. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class COMP4601A1_100846396_ClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
